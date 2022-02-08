ce82f1c6304118128d9bcc0146644df79abff1e5
agagotowiec <92727370+agagotowiec@users.noreply.github.com>
2022-02-08 19:44:57 +0000

    added synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,11 +1,25 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
 	return (
 		<div className="Meaning">
 			<h4>{props.meaning.partOfSpeech}</h4>
-			<p> {props.meaning.definitions[0].definition}</p>
-			<p>{props.meaning.definitions[0].example}</p>
+			{props.meaning.definitions.map(function (definition, index) {
+				return (
+					<div key={index}>
+						<p>{definition.definition}</p>
+
+						<em>
+							<p>Example: {definition.example}</p>
+						</em>
+						<br />
+						<p>
+							<Synonyms />
+						</p>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
