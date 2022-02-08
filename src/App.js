36897a38c5763e676d6288bae9b026a0dbc87c50
agagotowiec <92727370+agagotowiec@users.noreply.github.com>
2022-02-08 15:51:22 +0000

    added footer and footer styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>Hello</p>
-			</header>
+			<div className="container">
+				<header className="App-header">
+					<p>Hello</p>
+				</header>
+				<main>
+					<Dictionary />
+				</main>
+				<footer className="text-center">
+					This project was coded by
+					<strong>
+						<a href="https://www.linkedin.com/in/aga-got/" target={"_blank"} rel="noreferrer">
+							{" "}
+							Aga
+						</a>
+					</strong>{" "}
+					and is
+					<a
+						href="https://github.com/agagotowiec/dictionary-react"
+						target={"_blank"}
+						rel="noreferrer"
+					>
+						{" "}
+						open-sourced.
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
